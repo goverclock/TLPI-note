584b295d8ded5afb409ee2fabc7fcd098db7c205
goverclock <goverclock@outlook.com>
2023-01-14 11:09:51 +0800

    4.1 done

--- 4/4.1.cpp
+++ 4/4.1.cpp
@@ -5,23 +5,46 @@ tee 命令会将其覆盖。如文件已存在，请实现-a 命令行选项（t
 追加数据。（请参考附录 B 中对 getopt()函数的描述来解析命令行选项。 */
 
 #include "tlpi_hdr.h"
+#include <fcntl.h>
 #include <unistd.h>
 
+#define BUF_SIZE 1024
+
 int main(int argc, char *argv[]) {
-    if (argc > 3 || (argc > 1 && strcmp(argv[1], "--help")) == 0) {
+    if (argc > 3 || (argc > 1 && strcmp(argv[1], "--help") == 0)) {
         usageErr("%s [[-a] file]\n", argv[0]);
     }
+    
+    int fd = -1;
     
     int opt;
     while((opt = getopt(argc, argv, ":a:")) != -1) {
         if (opt == '?') {
-            printf("unrecognized command line option '%c'\n", optopt);
+            errExit("unrecognized command line option '%c'\n", optopt);
         } else if (opt == ':') {
-            printf("missing arguments for '-%c'\n", optopt);
+            errExit("missing arguments for '-%c'\n", optopt);
+        } else {    // opt == 'a'
+            fd = open(optarg, O_WRONLY | O_CREAT | O_APPEND);
+            if (fd == -1) {
+                errExit("open");
+            }
+        }
+    }
+    if (fd == -1 && argc > 1) {
+        fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC);
+        if (fd == -1) {
+            errExit("open");
         }
     }
 
-
+    char buf[BUF_SIZE];
+    int num_read = 0;
+    while((num_read = read(STDIN_FILENO, buf, BUF_SIZE)) > 0) {
+        write(STDOUT_FILENO, buf, num_read);
+        if (fd != -1) {
+            write(fd, buf, num_read);
+        }
+    }
 
     return 0;
 }
